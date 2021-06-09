489f3a8461766cdd6424e85f1f95f236222a5178
Shigi_p <shigi.522@gmail.com>
2021-06-09 10:41:08 +0900

    [add] textcomponent系追加、ファイル構成修正

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ const config = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, "css-loader", "sass-loader", "import-glob-loader"],
       },
       // {
       //   test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
