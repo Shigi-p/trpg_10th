84fc6fcbed61d58c28acfc4e1e04ebb583960485
Shigi_p <shigi.522@gmail.com>
2021-06-10 18:21:26 +0900

    [add] swiper追加、その他設定追加

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,11 @@
+import { Swiper } from "swiper";
+import "swiper/swiper-bundle.css";
+
+const swiper = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
+
 console.log("Hello World!");
