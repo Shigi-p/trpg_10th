8af5b9de345c641b891e9a8675aed1aa05db4f74
Shigi-p <shigi.522@gmail.com>
2021-11-14 18:27:13 +0900

    feat: ルーティング追加

---
+++ src/pages/characters/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+type Props = {};
+
+export const Characters: React.VFC<Props> = (props) => {
+  return (
+    <div className="p-characters">
+      <h1>Characters</h1>
+    </div>
+  );
+};
